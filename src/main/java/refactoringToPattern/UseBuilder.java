572bc7949dd8830d93169a99e79b7009981cd5b1
Yansen Dou <sendy_dou@hotmail.com>
2018-10-12 23:36:22 +0800

    [@ysdou] #n/a init project

---
+++ src/main/java/refactoringToPattern/UseBuilder.java
@@ -0,0 +1,5 @@
+package refactoringToPattern;
+
+public class UseBuilder {
+
+}
---
+++ src/test/java/refactoringToPattern/UseBuilderTest.java
@@ -0,0 +1,13 @@
+package refactoringToPattern;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UseBuilderTest {
+
+    @Test
+    void should_be_ok() {
+        assertTrue(true);
+    }
+}
