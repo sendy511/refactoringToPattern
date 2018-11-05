29bc8b8afab241c4299122d8e99fa6879fc223ec
Yansen Dou <sendy_dou@hotmail.com>
2018-11-05 23:52:36 +0800

    [@ysdou] #n/a add strategy no pattern

---
+++ src/main/java/refactoringToPattern/strategy/noPattern/Loan.java
@@ -0,0 +1,66 @@
+package refactoringToPattern.strategy.noPattern;
+
+import java.util.Date;
+
+public class Loan {
+
+    private Date expiry;
+    private Double maturity;
+    private double commitment;
+    private double outStanding;
+    private double unUsedPercentage;
+
+    public double captical(){
+        if (expiry == null && maturity != null) {
+            return commitment * duration() * riskFactor();
+        }
+
+        if (expiry != null && maturity == null) {
+            if (getUnusedPercentage() != 1.0) {
+                return commitment * getUnusedPercentage() * duration() * riskFactor();
+            } else {
+                return (outstandingRiskAmount() * duration() * riskFactor())
+                + (unusedRiskAmount() * duration() * unusedRiskFactor());
+            }
+        }
+
+        return 0.0;
+    }
+
+    private double unusedRiskFactor() {
+        return 100;
+    }
+
+    private double unusedRiskAmount() {
+        return commitment - outStanding;
+    }
+
+    private double outstandingRiskAmount() {
+        return outStanding;
+    }
+
+    private double getUnusedPercentage() {
+        return this.unUsedPercentage;
+    }
+
+    private double riskFactor() {
+        return 50;
+    }
+
+    private double duration() {
+        if (expiry == null && maturity != null) {
+            return weightedDuration();
+        } else {
+            return yearsTo(expiry);
+        }
+    }
+
+    private double yearsTo(Date expiry) {
+        return expiry.getYear();
+    }
+
+    private double weightedDuration() {
+        return expiry.getYear() * 2;
+    }
+
+}
